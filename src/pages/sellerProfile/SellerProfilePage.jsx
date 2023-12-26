f7f8be8e6c0f61e05b65626cddc696d0ded34095
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-12-26 16:37:14 +0300

    added card blog

--- src/pages/sellerProfile/SellerProfilePage.jsx
+++ src/pages/sellerProfile/SellerProfilePage.jsx
@@ -1,69 +1,50 @@
 import styles from "./styles.module.css";
 import LogoBlog from "../../components/logoBlog/LogoBlog";
-
+import CardItem from "../../components/card/Card";
 export default function SellerProfilePage() {
   return (
-<main className={styles.main}>
-
-        <div className={styles.mainCenterBlock}>
-  <LogoBlog/>
-          <h2 className={styles.mainH2}>Профиль продавца</h2>
-          <div className={`${styles.mainProfileSell} ${styles.profileSell}`}>
-            <div className={styles.profileSellContent}>
-              <div className={`${styles.profileSellSeller} ${styles.seller}`}>
-                <div className={styles.sellerLeft}>
-                  <div className={styles.sellerImg}>
+    <main className={styles.main}>
+      <div className={styles.mainCenterBlock}>
+        <LogoBlog />
+        <h2 className={styles.mainH2}>Профиль продавца</h2>
+        <div className={`${styles.mainProfileSell} ${styles.profileSell}`}>
+          <div className={styles.profileSellContent}>
+            <div className={`${styles.profileSellSeller} ${styles.seller}`}>
+              <div className={styles.sellerLeft}>
+                <div className={styles.sellerImg}>
+                  <a href="" target="_self">
+                    <img src="#" alt="" />
+                  </a>
+                </div>
+              </div>
+              <div className={styles.sellerRight}>
+                <h3 className={styles.sellerTitle}>Кирилл Матвеев</h3>
+                <p className={styles.sellerCity}>Санкт-Петербург</p>
+                <p className={styles.sellerInf}>
+                  Продает товары с августа 2021
+                </p>
+                <div className={styles.sellerImgMobBlock}>
+                  <div className={styles.sellerImgMob}>
                     <a href="" target="_self">
                       <img src="#" alt="" />
                     </a>
                   </div>
                 </div>
-                <div className={styles.sellerRight}>
-                  <h3 className={styles.sellerTitle}>Кирилл Матвеев</h3>
-                  <p className={styles.sellerCity}>Санкт-Петербург</p>
-                  <p className={styles.sellerInf}>Продает товары с августа 2021</p>
-                  <div className={styles.sellerImgMobBlock}>
-                    <div className={styles.sellerImgMob}>
-                      <a href="" target="_self">
-                        <img src="#" alt="" />
-                      </a>
-                    </div>
-                  </div>
-                  <button className={`${styles.sellerBtn} ${styles.btnHov02}`}>
-                    Показать&nbsp;телефон
-                    <span>8&nbsp;905&nbsp;ХХХ&nbsp;ХХ&nbsp;ХХ</span>
-                  </button>
-                </div>
+                <button className={`${styles.sellerBtn} ${styles.btnHov02}`}>
+                  Показать&nbsp;телефон
+                  <span>8&nbsp;905&nbsp;ХХХ&nbsp;ХХ&nbsp;ХХ</span>
+                </button>
               </div>
             </div>
           </div>
-          <h3 className={styles.mainTitle}>Товары продавца</h3>
         </div>
-        <div className={styles.mainContent}>
-          <div className={`${styles.contentCards} ${styles.cards}`}>
-            <div className={styles.cardsItem}>
-              <div className={`${styles.cardsCard} ${styles.card}`}>
-                <div className={styles.cardImage}>
-                  <a href="" target="_blank">
-                    <img src="#" alt="picture" />
-                  </a>
-                </div>
-                <div className={styles.cardContent}>
-                  <a href="" target="_blank">
-                    <h3 className={styles.cardTitle}>
-                      Ракетка для большого тенниса Triumph Pro ST
-                    </h3>
-                  </a>
-                  <p className={styles.cardPrice}>2&nbsp;200&nbsp;₽</p>
-                  <p className={styles.cardPlace}>Санкт Петербург</p>
-                  <p className={styles.cardDate}>Сегодня в&nbsp;10:45</p>
-                </div>
-              </div>
-            </div>
-            {/* Добавьте остальные карточки сюда */}
-          </div>
+        <h3 className={styles.mainTitle}>Товары продавца</h3>
+      </div>
+      <div className={styles.mainContent}>
+        <div className={`${styles.contentCards} ${styles.cards}`}>
+          <CardItem />
         </div>
-
+      </div>
     </main>
   );
-};
+}
