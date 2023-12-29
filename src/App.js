ad59363f8960e41c6c6729b935d90cd2c868551b
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-12-29 23:14:09 +0300

    added get info from local

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import AppRoutes from "./routes";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { initializeUserFromLocalStorage } from "./store/slices/userSlice.js";
+
+
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // При монтировании компонента, диспетчеризуем действие для инициализации из localStorage
+    dispatch(initializeUserFromLocalStorage());
+  }, [dispatch]);
+
   return (
     <>
         <AppRoutes />
