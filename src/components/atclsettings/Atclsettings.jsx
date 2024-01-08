c967a8058d6b29f01d3b1c2510d054e4b0288ddc
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-01-08 13:18:26 +0300

    отобржение картинок в форме редактирования

--- src/components/atclsettings/Atclsettings.jsx
+++ src/components/atclsettings/Atclsettings.jsx
@@ -5,15 +5,17 @@ import { useUpdateProductMutation } from "../../store/RTKQuery/adsApi";
 
 const Atclsettings = ({ setIsShowSettings, adv }) => {
   console.log(adv);
-  const id = adv.id
+  const id = adv.id;
   const [nameAdv, setNameAdv] = useState(adv?.title);
   const [descriptionAdv, setDescriptionAdv] = useState(adv?.description);
   const [priceAdv, setPriceAdv] = useState(adv?.price);
   const [errorForm, setErrorForm] = useState(null);
   const [offButton, setOffButton] = useState(true);
   const navigate = useNavigate();
-  const [updateProduct, { isLoading, error }] =
-    useUpdateProductMutation();
+  const [updateProduct, { isLoading, error }] = useUpdateProductMutation();
+
+  const [showingImg, setShowingImg] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
 
   useEffect(() => {
     if (!nameAdv || !descriptionAdv || !priceAdv) {
@@ -40,8 +42,7 @@ const Atclsettings = ({ setIsShowSettings, adv }) => {
       navigate(`/myartycle/${response.data?.id}`);
       setIsShowSettings(false);
     } catch (err) {
-      // Handle errors if needed
-      console.error("Add product error:", err);
+      setErrorForm(err);
     }
   };
 
@@ -49,8 +50,9 @@ const Atclsettings = ({ setIsShowSettings, adv }) => {
     <div className={styles.containerBg}>
       <div className={styles.modalBlock}>
         <div className={styles.modalContent}>
-        <div className={styles.modalTitleBlog}>
-            <svg className={styles.articleFillImgSvg}
+          <div className={styles.modalTitleBlog}>
+            <svg
+              className={styles.articleFillImgSvg}
               xmlns="http://www.w3.org/2000/svg"
               width="12"
               height="21"
@@ -60,13 +62,9 @@ const Atclsettings = ({ setIsShowSettings, adv }) => {
                 setIsShowSettings(false);
               }}
             >
-              <path
-                d="M11 1.5L2 10.5L11 19.5"
-                stroke="black"
-                strokeWidth="2"
-              />
+              <path d="M11 1.5L2 10.5L11 19.5" stroke="black" strokeWidth="2" />
             </svg>
-            </div>
+          </div>
           <h3 className={styles.modalTitle}>Редактировать объявление</h3>
           <div className={styles.modalBtnClose}>
             <div
@@ -117,32 +115,22 @@ const Atclsettings = ({ setIsShowSettings, adv }) => {
                 }}
               />
             </div>
-            <div className={styles.formNewArtBlock}>
-              <p className={styles.formNewArtP}>
-                Фотографии товара<span>не более 5 фотографий</span>
-              </p>
-              <div className={styles.formNewArtBarImg}>
-                <div className={styles.formNewArtImg}>
-                  <img src="" alt="" />
-                  <div className={styles.formNewArtImgCover}></div>
+            <div className={styles.formNewArtBarImg}>
+              {adv.images.map((image, index) => (
+                <div key={index} className={styles.formNewArtImg}>
+                  <img src={`http://localhost:8090/${image.url}`} alt={`Image ${index + 1}`} />
+                  <div className={styles.formNewArtImgCover} />
                 </div>
-                <div className={styles.formNewArtImg}>
-                  <img src="" alt="" />
-                  <div className={styles.formNewArtImgCover}></div>
-                </div>
-                <div className={styles.formNewArtImg}>
-                  <div className={styles.formNewArtImgCover}></div>
-                  <img src="" alt="" />
-                </div>
-                <div className={styles.formNewArtImg}>
-                  <div className={styles.formNewArtImgCover}></div>
-                  <img src="" alt="" />
-                </div>
-                <div className={styles.formNewArtImg}>
-                  <div className={styles.formNewArtImgCover}></div>
-                  <img src="" alt="" />
-                </div>
-              </div>
+              ))}
+              {/* Добавление пустых блоков для недостающих изображений */}
+              {Array.from({ length: Math.max(5 - adv.images.length, 0) }).map(
+                (_, index) => (
+                  <div key={index} className={styles.formNewArtImg}>
+                    <div className={styles.formNewArtImgCover}></div>
+                    <img src="" alt="" />
+                  </div>
+                )
+              )}
             </div>
             <div className={`${styles.formNewArtBlock} ${styles.blockPrice}`}>
               <label className={styles.formLabel} htmlFor="price">
@@ -162,7 +150,11 @@ const Atclsettings = ({ setIsShowSettings, adv }) => {
             </div>
 
             <button
-              className={offButton? (`${styles.formNewArtBtnPub}`) : (`${styles.formNewArtBtnPubActive} ${styles.btnHov02}`)}
+              className={
+                offButton
+                  ? `${styles.formNewArtBtnPub}`
+                  : `${styles.formNewArtBtnPubActive} ${styles.btnHov02}`
+              }
               id="btnPublish"
               disabled={offButton}
               type="submit"
